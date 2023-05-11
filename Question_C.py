d193bd46005a132e3efe6a3567c7486be61b3749
kjk-poe <kuajiakang@gmail.com>
2023-05-11 18:51:06 +0800

    update questionc

--- Question_C.py
+++ Question_C.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 def main():
-    df = pd.read_csv(r'.\files\http.log', delimiter='\t',  error_bad_lines=False, names=['Date/time', 'hash/identity', 'ip_address_1', 'userid/identity', 'ip_address_2', 'timezone', 'method', 'ip_address_3','request_url', 'something1', 'something2', 'something3','something4', 'status code', 'something5', 'something6', 'something7','something8','something9', 'something10', 'something11','something12','something13', 'something14', 'something15','something16','something17'])
+    df = pd.read_csv(r'.\files\http.log', delimiter='\t',  error_bad_lines=False, names=['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p',
+                                                                                          'trans_depth', 'method','host', 'uri', 'referrer', 'user_agent',
+                                                                                          'request_ body_len', 'response_ body_len', 'status_code', 'status_msg',
+                                                                                          'info_code','info_msg','filename', 'tags', 'username','password','proxied',
+                                                                                          'orig_fuids', 'orig_mime_types','resp_fuids','resp_mime_types'])
     print(df)
 
 if __name__ == "__main__":
